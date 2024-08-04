9bc2f7e5dfbab2001a4b3f48da19611b92024633
Brandon Gowera <gowerabrandon@gmail.com>
2024-08-04 13:16:41 +0200

    added validation for user prompt

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const main=document.querySelector('.main')
+const main = document.querySelector(".main");
 const gridContainer = document.querySelector(".grid-container");
 const colorPicker = document.querySelector(".color-picker");
 const clearGridBtn = document.querySelector(".clear-grid-btn");
@@ -10,10 +10,13 @@ const opacityModeBtn = document.querySelector(".opacity-mode-btn");
 function createBoxes(numberOfBoxes) {
 	let counter = numberOfBoxes * numberOfBoxes;
 	let basisCalculation = (numberOfBoxes / counter) * 100;
+
 	for (let i = 0; i < counter; i++) {
 		let gridDiv = document.createElement("div");
 		gridDiv.classList.add("grid-item");
 		gridDiv.style.flexBasis = `${basisCalculation}%`;
+		//functions
+
 		function penMode() {
 			gridDiv.addEventListener("mouseenter", () => {
 				gridDiv.style.backgroundColor = colorPicker.value;
@@ -22,33 +25,37 @@ function createBoxes(numberOfBoxes) {
 		function clear() {
 			gridDiv.style.backgroundColor = "";
 		}
-		clearGridBtn.addEventListener("click", clear);
-		penBtn.addEventListener("click", penMode);
+		//event listeners
+
 		eraseBtn.addEventListener("click", () => {
 			gridDiv.addEventListener("mouseenter", () => {
 				clear();
 			});
 		});
-		opacityModeBtn.addEventListener('click', () => {
-			gridDiv.addEventListener('mouseenter', () => {
-				gridDiv.classList.add('opacity')
-			})
-		})
-
+		clearGridBtn.addEventListener("click", clear);
+		penBtn.addEventListener("click", penMode);
+		//function end
+		
 		gridContainer.appendChild(gridDiv);
 	}
 }
 
 createBoxes(16);
-	
+
 function gridSizeChange(input) {
 	gridContainer.replaceChildren();
-	input = parseInt(prompt("What would you like your grid size to be? Pick any number between 1 and 100"));
+	input = parseInt(
+		prompt(
+			"What would you like your grid size to be? Pick any number between 1 and 100"
+		)
+	);
 	if (input > 100 || input < 1) {
-		alert('Invalid Number! Please press the button again to pick a number between 1 and 100');
+		alert(
+			"Invalid Number! Please press the button again to pick a number between 1 and 100"
+		);
 		return;
 	}
 	return createBoxes(input);
 }
 
-changeGridSizeBtn.addEventListener('click', gridSizeChange);
+changeGridSizeBtn.addEventListener("click", gridSizeChange);
